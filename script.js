7ad1f97505a5568665c40426eb3f1c77bbe80d92
RheaSudesh <vrhea304@gmail.com>
2021-02-14 12:22:35 +0530

    1528832

--- script.js
+++ script.js
@@ -9,6 +9,33 @@
 // the function should return true if all the conditions above are validated.
 // return false if the validation fails
 
+function checkValidate() {
+    checkUserName();
+    checkEmail();
+    checkPassword();
+    if((checkUserName() && checkEmail()) && checkPassword() )
+    {
+        alert("Sucessful Login");
+    }
+}
+
+function checkUserName() {
+    var username = document.getElementById('usertext').value;
+    var specialChar = /[+\-=\[\]{};':"\\|,.<>\/?!@#$%^&*()_]+/; 
+    if (usertext == null || username.length < 8 || username.length > 15
+        || username.split('').includes(' ')) {
+        alert('Please enter valid Username of length 8 to 15 and with no space')
+        return false;
+    }
+    else if (specialChar.test(username)) {
+        alert('Please enter valid Username without any special Characters')
+        return false;
+    }
+    else {
+        return true;
+    }
+}
+
 // Progression 2 
 // Check mail
 // create a function called as checkEmail()
@@ -16,9 +43,37 @@
 // It can be alphanumeric in nature
 // return true if it passses all validation and false otherwise
 
+function checkEmail() {
+    let mailId = document.getElementById('emailtext').value;
+    var specialChar = /[+\-=\[\]{};':"\\|,<>\/?!#$%^&*()]+/; //special characters 
+    if (mailId == null) {
+        alert('Please enter valid MailID!')
+        return false;
+    }
+    else if (specialChar.test(mailId)) {
+        alert('Please enter valid MailID!')
+        return false;
+    }
+    else {
+        return true;
+    }
+}
+
 // Progression 3
 // Check password
 // create a function called as checkPassword()
 // should be alphanumeric in nature
 // password must contain atleast one Uppercase, one number and special characters[!@#$%^&*()_]
 // return true else return false
+
+function checkPassword() {
+    var password = document.getElementById('passwordtext').value;
+    if (password.match(/[a-z]/g) && password.match(/[A-Z]/g) && password.match(/[0-9]/g)
+        && password.match(/[^a-zA-Z\d]/g)) {
+        return true;
+    }
+    else {
+        alert('Please enter valid password with \nAtleast one UpperCase \nAtleast one Lowercase \nAtleast one special Character');
+        return true;
+    }
+}
